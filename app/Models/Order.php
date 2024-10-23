a3251ed5b01d534467fa1153ee6d921b92f30bbc
EliaxZen <galvaoalveselias@gmail.com>
2024-10-23 14:21:57 -0300

    criando models e migrations de products e orders

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome_recebedor',
+        'cep',
+        'endereco',
+        'numero',
+        'bairro',
+        'cidade',
+        'estado',
+        'complemento',
+    ];
+
+    public function products() 
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ database/migrations/2024_10_23_170304_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_recebedor');
+            $table->string('cep');
+            $table->string('endereco');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->text('complemento')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
